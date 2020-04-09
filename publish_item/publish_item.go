2fda1121205c9e28975e4f24c574088a41331ac2
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-09 11:25:01 +0200

    update

--- publish_item/publish_item.go
+++ publish_item/publish_item.go
@@ -42,6 +42,35 @@ func main() {
     item := items.Item{
         Id:     "123",
         Source: "ikea",
+        Lang:   "en",
+        Urls: []string{
+            "https://www.ikea.com/gb/en/p/leifarne-swivel-chair-dark-yellow-balsberget-white-s29301700/",
+        },
+        Name:        "LEIFARNE Swivel chair - dark yellow, Balsberget white",
+        Description: "LEIFARNE Swivel chair - dark yellow, Balsberget white. You sit comfortably thanks to the restful flexibility of the scooped seat and shaped back. The self-adjusting plastic feet adds stability to the chair. A special surface treatment on the seat prevents you from sliding.",
+        Categories: []string{
+            "Dining chairs",
+        },
+        ImageUrls: []string{
+            "https://www.ikea.com/gb/en/images/products/leifarne-swivel-chair__0742962_PE742882_S5.JPG",
+            "https://www.ikea.com/gb/en/images/products/leifarne-swivel-chair__0802274_PH165465_S5.JPG",
+            "https://www.ikea.com/gb/en/images/products/leifarne-swivel-chair__0799469_PH165466_S5.JPG",
+            "https://www.ikea.com/gb/en/images/products/leifarne-swivel-chair__0742966_PE742886_S5.JPG",
+            "https://www.ikea.com/gb/en/images/products/leifarne-swivel-chair__0742965_PE742883_S5.JPG",
+            "https://www.ikea.com/gb/en/images/products/leifarne-swivel-chair__0787512_PE763253_S5.JPG",
+        },
+        Dimensions: map[string]string{
+            "Tested for: ":       "100 kg",
+            "Width: ":            "69 cm",
+            "Depth: ":            "69 cm",
+            "Max. height: ":      "87 cm",
+            "Seat width: ":       "45 cm",
+            "Seat depth: ":       "36 cm",
+            "Min. seat height: ": "41 cm",
+            "Max. seat height: ": "51 cm",
+        },
+        Price:         31.0,
+        PriceCurrency: "GBP",
     }
 
     body, err := proto.Marshal(&item)
